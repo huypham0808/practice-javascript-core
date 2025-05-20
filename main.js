7d7f0125c79462c60137fd3f8eca59b7f835ee21
Huydev0808 <phamngohoanghuy95@gmail.com>
2025-05-20 12:51:28 +0700

    practice javascript core

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 function theFirt(callback) {
-    setTimeout(function(){
+    setTimeout(function () {
         console.log("theFirt");
         callback();
     }, 1000)
 };
-function theSecond () {
+function theSecond() {
     console.log("theSecond");
 };
 
-theFirt(theSecond);
+// theFirt(theSecond);
 
 //Xử lý bất đồng bộ => Cho đồng bộ trở lại
 //CACH 1: CallbackFunction (Hàm có tham số là 1 hàm khác)
+
+/**2025-05-19 - LEARNING JAVASCRIPT CORE */
+const test = () => {
+    console.log(123);
+}
+test();
+
+const changeStyle = () => {
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "blue";
+    document.getElementById("demo").style.fontSize = "30px";
+}
